06bc4ab09c581ae2cf9c236bafda73f3bf91ac7f
Roberto Novelo <robertod.novelo@gmail.com>
2014-11-12 16:23:21 -0600

    CodeIgniter Database Configuration
    Configuration values for CodeIginiter application set, for it to be
    able to connect to the database.

--- application/config/database.php
+++ application/config/database.php
@@ -49,12 +49,12 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
-$db['default']['dbdriver'] = 'mysql';
+$db['default']['username'] = 'delfuego';
+$db['default']['password'] = 'Simowpri2013';
+$db['default']['database'] = 'delfuegoshop';
+$db['default']['dbdriver'] = 'mysqli';
 $db['default']['dbprefix'] = '';
-$db['default']['pconnect'] = TRUE;
+$db['default']['pconnect'] = FALSE;
 $db['default']['db_debug'] = TRUE;
 $db['default']['cache_on'] = FALSE;
 $db['default']['cachedir'] = '';
