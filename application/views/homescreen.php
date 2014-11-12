8af54f42b48aa33d8d353629fadb37a8ebd7f6f8
Roberto Novelo <robertod.novelo@gmail.com>
2014-11-12 11:59:53 -0600

    Welcome Controller Calls First View Sketch
    Default site controller now calls the home screen view, which holds a
    sample of the final website.

---
+++ application/views/homescreen.php
@@ -0,0 +1,350 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+	<head>
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+		<title>DelFuego Shop</title>
+		<meta name="description" content="Loading Effects for Grid Items with CSS Animations" />
+		<meta name="keywords" content="css animation, loading effect, google plus, grid items, masonry" />
+		<meta name="author" content="Codrops" />
+		<link rel="shortcut icon" href="../favicon.ico"> 
+		<link rel="stylesheet" type="text/css" href="css/default.css" />
+		<link rel="stylesheet" type="text/css" href="css/component.css" />
+		<link rel="stylesheet" type="text/css" href="css/hover.css" />
+		<link rel="stylesheet" type="text/css" href="css/site.css" />
+		<script src="js/modernizr.custom.js"></script>
+	</head>
+	<body>
+		<div class="container">
+			<!-- Top Navigation -->
+			<header>
+				<h1>DelFuego<span>Shop</span></h1>	
+				<nav class="codrops-demos">
+					<a class="menu-item">T-Shirts</a>
+					<a class="menu-item">Tanks</a>
+					<a class="current-demo">Shorts</a>
+					<a class="menu-item">Blazers</a>
+				</nav>
+			</header>
+			<ul class="grid effect-3" id="grid">
+				
+				<li>
+					<div class="item-holder">
+						<div class="hover_block block_2">
+			                <img class="item-image" src="images/FRSC-DelFuegoTshirt.jpg">
+			                <div class="hover_info">
+			                    <h3 class="">DelFuego T-Shirt</h3>
+			                    <div class="add-to-cart">Read more</div>
+			                </div>
+			            </div>
+
+			            <div class="item-details">
+			            	<p class="item-name">Delfuego T-shirt</p>
+			            	<p class="item-price">$35USD</p>
+			            </div>
+					</div>
+				</li>
+				<li>
+					<div class="item-holder">
+						<div class="hover_block block_2">
+			                <img class="item-image" src="images/FRSC-DelFuegoTshirt.jpg">
+			                <div class="hover_info">
+			                    <h3 class="">DelFuego T-Shirt</h3>
+			                    <div class="add-to-cart">Read more</div>
+			                </div>
+			            </div>
+
+			            <div class="item-details">
+			            	<p class="item-name">Delfuego T-shirt</p>
+			            	<p class="item-price">$35USD</p>
+			            </div>
+					</div>
+				</li>
+
+				<li>
+					<div class="item-holder">
+						<div class="hover_block block_2">
+			                <img class="item-image" src="images/FRSC-DelFuegoTshirthalf.jpg">
+			                <div class="hover_info">
+			                    <h3 class="">DelFuego T-Shirt</h3>
+			                    <div class="add-to-cart">Read more</div>
+			                </div>
+			            </div>
+
+			            <div class="item-details">
+			            	<p class="item-name">Delfuego T-shirt</p>
+			            	<p class="item-price">$35USD</p>
+			            </div>
+					</div>
+				</li>
+
+				<li>
+					<div class="item-holder">
+						<div class="hover_block block_2">
+			                <img class="item-image" src="images/FRSC-DelFuegoTshirt.jpg">
+			                <div class="hover_info">
+			                    <h3 class="">DelFuego T-Shirt</h3>
+			                    <div class="add-to-cart">Read more</div>
+			                </div>
+			            </div>
+
+			            <div class="item-details">
+			            	<p class="item-name">Delfuego T-shirt</p>
+			            	<p class="item-price">$35USD</p>
+			            </div>
+					</div>
+				</li>
+				<li>
+					<div class="item-holder">
+						<div class="hover_block block_2">
+			                <img class="item-image" src="images/FRSC-DelFuegoTshirt.jpg">
+			                <div class="hover_info">
+			                    <h3 class="">DelFuego T-Shirt</h3>
+			                    <div class="add-to-cart">Read more</div>
+			                </div>
+			            </div>
+
+			            <div class="item-details">
+			            	<p class="item-name">Delfuego T-shirt</p>
+			            	<p class="item-price">$35USD</p>
+			            </div>
+					</div>
+				</li>
+				<li>
+					<div class="item-holder">
+						<div class="hover_block block_2">
+			                <img class="item-image" src="images/FRSC-DelFuegoTshirt.jpg">
+			                <div class="hover_info">
+			                    <h3 class="">DelFuego T-Shirt</h3>
+			                    <div class="add-to-cart">Read more</div>
+			                </div>
+			            </div>
+
+			            <div class="item-details">
+			            	<p class="item-name">Delfuego T-shirt</p>
+			            	<p class="item-price">$35USD</p>
+			            </div>
+					</div>
+				</li>
+
+				<li>
+					<div class="item-holder">
+						<div class="hover_block block_2">
+			                <img class="item-image" src="images/FRSC-DelFuegoTshirthalf.jpg">
+			                <div class="hover_info">
+			                    <h3 class="">DelFuego T-Shirt</h3>
+			                    <div class="add-to-cart">Read more</div>
+			                </div>
+			            </div>
+
+			            <div class="item-details">
+			            	<p class="item-name">Delfuego T-shirt</p>
+			            	<p class="item-price">$35USD</p>
+			            </div>
+					</div>
+				</li>
+
+				<li>
+					<div class="item-holder">
+						<div class="hover_block block_2">
+			                <img class="item-image" src="images/FRSC-DelFuegoTshirt.jpg">
+			                <div class="hover_info">
+			                    <h3 class="">DelFuego T-Shirt</h3>
+			                    <div class="add-to-cart">Read more</div>
+			                </div>
+			            </div>
+
+			            <div class="item-details">
+			            	<p class="item-name">Delfuego T-shirt</p>
+			            	<p class="item-price">$35USD</p>
+			            </div>
+					</div>
+				</li>
+				<li>
+					<div class="item-holder">
+						<div class="hover_block block_2">
+			                <img class="item-image" src="images/FRSC-DelFuegoTshirt.jpg">
+			                <div class="hover_info">
+			                    <h3 class="">DelFuego T-Shirt</h3>
+			                    <div class="add-to-cart">Read more</div>
+			                </div>
+			            </div>
+
+			            <div class="item-details">
+			            	<p class="item-name">Delfuego T-shirt</p>
+			            	<p class="item-price">$35USD</p>
+			            </div>
+					</div>
+				</li>
+				<li>
+					<div class="item-holder">
+						<div class="hover_block block_2">
+			                <img class="item-image" src="images/FRSC-DelFuegoTshirt.jpg">
+			                <div class="hover_info">
+			                    <h3 class="">DelFuego T-Shirt</h3>
+			                    <div class="add-to-cart">Read more</div>
+			                </div>
+			            </div>
+
+			            <div class="item-details">
+			            	<p class="item-name">Delfuego T-shirt</p>
+			            	<p class="item-price">$35USD</p>
+			            </div>
+					</div>
+				</li>
+
+				<li>
+					<div class="item-holder">
+						<div class="hover_block block_2">
+			                <img class="item-image" src="images/FRSC-DelFuegoTshirthalf.jpg">
+			                <div class="hover_info">
+			                    <h3 class="">DelFuego T-Shirt</h3>
+			                    <div class="add-to-cart">Read more</div>
+			                </div>
+			            </div>
+
+			            <div class="item-details">
+			            	<p class="item-name">Delfuego T-shirt</p>
+			            	<p class="item-price">$35USD</p>
+			            </div>
+					</div>
+				</li>
+
+				<li>
+					<div class="item-holder">
+						<div class="hover_block block_2">
+			                <img class="item-image" src="images/FRSC-DelFuegoTshirt.jpg">
+			                <div class="hover_info">
+			                    <h3 class="">DelFuego T-Shirt</h3>
+			                    <div class="add-to-cart">Read more</div>
+			                </div>
+			            </div>
+
+			            <div class="item-details">
+			            	<p class="item-name">Delfuego T-shirt</p>
+			            	<p class="item-price">$35USD</p>
+			            </div>
+					</div>
+				</li>
+				<li>
+					<div class="hover_block block_2">
+		                <img src="images/4.jpg">
+		                <div class="hover_info">
+		                    <h1>Article Title</h1>
+		                    <p>Lorem ipsum dolor. Aenean scelerisque odio ut dui feugiat commodo. Nulla blandit erat vel nisi consectetur ac pharetra augue consectetur.</p>
+		                    <a href="">Read more</a>
+		                </div>
+		            </div>
+				</li>
+				<li>
+					<div class="hover_block block_2">
+		                <img src="images/3.jpg">
+		                <div class="hover_info">
+		                    <h1>Article Title</h1>
+		                    <p>Lorem ipsum dolor. Aenean scelerisque odio ut dui feugiat commodo. Nulla blandit erat vel nisi consectetur ac pharetra augue consectetur.</p>
+		                    <a href="">Read more</a>
+		                </div>
+		            </div>
+				</li>
+				<li><a href="http://drbl.in/fWPV"><img src="images/3.jpg"></a></li>
+				<li><a href="http://drbl.in/fWMT"><img src="images/4.jpg"></a></li>
+				<li><a href="http://drbl.in/fQdt"><img src="images/12.png"></a></li>
+				<li><a href="http://drbl.in/fHaa"><img src="images/13.png"></a></li>
+				<li><a href="http://drbl.in/gXMo"><img src="images/10.png"></a></li>
+				<li><a href="http://drbl.in/gXMn"><img src="images/9.jpg"></a></li>
+				<li><a href="http://drbl.in/fzYo"><img src="images/2.jpg"></a></li>
+				<li><a href="http://drbl.in/fARU"><img src="images/14.png"></a></li>
+				<li><a href="http://drbl.in/fGhI"><img src="images/5.jpg"></a></li>
+				<li><a href="http://drbl.in/fKCf"><img src="images/6.jpg"></a></li>
+				<li><a href="http://drbl.in/fLBG"><img src="images/7.jpg"></a></li>
+				<li><a href="http://drbl.in/fPEY"><img src="images/8.jpg"></a></li>
+				<li><a href="http://drbl.in/fWwG"><img src="images/9.jpg"></a></li>
+				<li><a href="http://drbl.in/fQdt"><img src="images/12.png"></a></li>
+				<li><a href="http://drbl.in/fHaa"><img src="images/13.png"></a></li>
+				<li><a href="http://drbl.in/gXMo"><img src="images/10.png"></a></li>
+				<li><a href="http://drbl.in/gXMn"><img src="images/11.png"></a></li>
+				<li><a href="http://drbl.in/fzYo"><img src="images/2.jpg"></a></li>
+				<li><a href="http://drbl.in/fARU"><img src="images/14.png"></a></li>
+				<li><a href="http://drbl.in/fWMM"><img src="images/1.jpg"></a></li>
+				<li><a href="http://drbl.in/fWPV"><img src="images/3.jpg"></a></li>
+				<li><a href="http://drbl.in/fWMT"><img src="images/4.jpg"></a></li>
+				<li><a href="http://drbl.in/fGhI"><img src="images/5.jpg"></a></li>
+				<li><a href="http://drbl.in/fKCf"><img src="images/6.jpg"></a></li>
+				<li><a href="http://drbl.in/fLBG"><img src="images/7.jpg"></a></li>
+				<li><a href="http://drbl.in/fPEY"><img src="images/8.jpg"></a></li>
+				<li><a href="http://drbl.in/fWwG"><img src="images/9.jpg"></a></li>
+				<li><a href="http://drbl.in/fQdt"><img src="images/12.png"></a></li>
+				<li><a href="http://drbl.in/fHaa"><img src="images/13.png"></a></li>
+				<li><a href="http://drbl.in/gXMo"><img src="images/10.png"></a></li>
+				<li><a href="http://drbl.in/gXMn"><img src="images/11.png"></a></li>
+				<li><a href="http://drbl.in/fzYo"><img src="images/2.jpg"></a></li>
+				<li><a href="http://drbl.in/fARU"><img src="images/14.png"></a></li>
+				<li><a href="http://drbl.in/fWMM"><img src="images/1.jpg"></a></li>
+				<li><a href="http://drbl.in/fWPV"><img src="images/3.jpg"></a></li>
+				<li><a href="http://drbl.in/fWMT"><img src="images/4.jpg"></a></li>
+				<li><a href="http://drbl.in/fGhI"><img src="images/5.jpg"></a></li>
+				<li><a href="http://drbl.in/fKCf"><img src="images/6.jpg"></a></li>
+				<li><a href="http://drbl.in/fLBG"><img src="images/7.jpg"></a></li>
+				<li><a href="http://drbl.in/fPEY"><img src="images/8.jpg"></a></li>
+				<li><a href="http://drbl.in/fWwG"><img src="images/9.jpg"></a></li>
+				<li><a href="http://drbl.in/fQdt"><img src="images/12.png"></a></li>
+				<li><a href="http://drbl.in/fHaa"><img src="images/13.png"></a></li>
+				<li><a href="http://drbl.in/gXMo"><img src="images/10.png"></a></li>
+				<li><a href="http://drbl.in/gXMn"><img src="images/11.png"></a></li>
+				<li><a href="http://drbl.in/fzYo"><img src="images/2.jpg"></a></li>
+				<li><a href="http://drbl.in/fARU"><img src="images/14.png"></a></li>
+				<li><a href="http://drbl.in/fWMM"><img src="images/1.jpg"></a></li>
+				<li><a href="http://drbl.in/fWPV"><img src="images/3.jpg"></a></li>
+				<li><a href="http://drbl.in/fWMT"><img src="images/4.jpg"></a></li>
+				<li><a href="http://drbl.in/fGhI"><img src="images/5.jpg"></a></li>
+				<li><a href="http://drbl.in/fKCf"><img src="images/6.jpg"></a></li>
+				<li><a href="http://drbl.in/fLBG"><img src="images/7.jpg"></a></li>
+				<li><a href="http://drbl.in/fPEY"><img src="images/8.jpg"></a></li>
+				<li><a href="http://drbl.in/fWwG"><img src="images/9.jpg"></a></li>
+				<li><a href="http://drbl.in/fQdt"><img src="images/12.png"></a></li>
+				<li><a href="http://drbl.in/fHaa"><img src="images/13.png"></a></li>
+				<li><a href="http://drbl.in/gXMo"><img src="images/10.png"></a></li>
+				<li><a href="http://drbl.in/gXMn"><img src="images/11.png"></a></li>
+				<li><a href="http://drbl.in/fzYo"><img src="images/2.jpg"></a></li>
+				<li><a href="http://drbl.in/fARU"><img src="images/14.png"></a></li>
+				<li><a href="http://drbl.in/fWMM"><img src="images/1.jpg"></a></li>
+				<li><a href="http://drbl.in/fWPV"><img src="images/3.jpg"></a></li>
+				<li><a href="http://drbl.in/fWMT"><img src="images/4.jpg"></a></li>
+				<li><a href="http://drbl.in/fGhI"><img src="images/5.jpg"></a></li>
+				<li><a href="http://drbl.in/fKCf"><img src="images/6.jpg"></a></li>
+				<li><a href="http://drbl.in/fLBG"><img src="images/7.jpg"></a></li>
+				<li><a href="http://drbl.in/fPEY"><img src="images/8.jpg"></a></li>
+				<li><a href="http://drbl.in/fWwG"><img src="images/9.jpg"></a></li>
+				<li><a href="http://drbl.in/fQdt"><img src="images/12.png"></a></li>
+				<li><a href="http://drbl.in/fHaa"><img src="images/13.png"></a></li>
+				<li><a href="http://drbl.in/gXMo"><img src="images/10.png"></a></li>
+				<li><a href="http://drbl.in/gXMn"><img src="images/11.png"></a></li>
+				<li><a href="http://drbl.in/fzYo"><img src="images/2.jpg"></a></li>
+				<li><a href="http://drbl.in/fARU"><img src="images/14.png"></a></li>
+				<li><a href="http://drbl.in/fWMM"><img src="images/1.jpg"></a></li>
+				<li><a href="http://drbl.in/fWPV"><img src="images/3.jpg"></a></li>
+				<li><a href="http://drbl.in/fWMT"><img src="images/4.jpg"></a></li>
+				<li><a href="http://drbl.in/fGhI"><img src="images/5.jpg"></a></li>
+				<li><a href="http://drbl.in/fKCf"><img src="images/6.jpg"></a></li>
+				<li><a href="http://drbl.in/fLBG"><img src="images/7.jpg"></a></li>
+				<li><a href="http://drbl.in/fPEY"><img src="images/8.jpg"></a></li>
+				<li><a href="http://drbl.in/fWwG"><img src="images/9.jpg"></a></li>
+				<li><a href="http://drbl.in/fWPV"><img src="images/3.jpg"></a></li>
+				<li><a href="http://drbl.in/fGhI"><img src="images/5.jpg"></a></li>
+				<li><a href="http://drbl.in/fzYo"><img src="images/2.jpg"></a></li>
+			</ul>
+		</div>
+		<script src="js/masonry.pkgd.min.js"></script>
+		<script src="js/imagesloaded.js"></script>
+		<script src="js/classie.js"></script>
+		<script src="js/AnimOnScroll.js"></script>
+		<script>
+			new AnimOnScroll( document.getElementById( 'grid' ), {
+				minDuration : 0.4,
+				maxDuration : 0.9,
+				viewportFactor : 0.2
+			} );
+		</script>
+	</body>
+</html>
