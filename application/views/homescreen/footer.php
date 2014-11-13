412ef5cf217189f8ad3e2f5bdcf16f5491bf1753
Roberto Novelo <robertod.novelo@gmail.com>
2014-11-13 11:06:11 -0600

    Details Modal Demo
    Details modal demo has been implemented. It shows a fullscreen col 8
    harcoded background image. It also has a nag bar functionality
    implemented to switch between product and shipping details.

--- application/views/homescreen/footer.php
+++ application/views/homescreen/footer.php
@@ -1,10 +1,77 @@
 			</ul>
 		</div>
+
+		<div class="modal fade" id="detailsModal" tabindex="-1" role="dialog" aria-labelledby="detailsModalLabel" aria-hidden="true">
+	        <div class="modal-dialog modal-lg">
+	            <div class="modal-content">
+	                <div class="modal-body">
+	                    <div class="row">
+	                    	<div class="col-xs-12 col-sm-8 detail-border">
+	                    		<div class="detail-img">
+	                    		</div>
+	                    	</div>
+	                    	<div class="col-xs-12 col-sm-4">
+
+	                    		<h4 class="detail-title">Feather FullPrint T-shirt</h4>
+	                    		<h4 class="detail-price">$40 <span>USD</span></h4>
+	                    		<div class="row detail-options">
+	                    			<div class="btn-group detail-size">
+										<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+											M <span class="caret"></span>
+										</button>
+										<ul class="dropdown-menu detail-size-options" role="menu">
+											<li><a>XS</a></li>
+											<li><a>S</a></li>
+											<li><a>M</a></li>
+											<li><a>L</a></li>
+											<li><a>XL</a></li>
+										</ul>
+									</div>
+	                    			<div class="col-xs-12 center-text">
+	                    				<button class="btn btn-addtobag">Add to bag</button>
+	                    			</div>
+	                    		</div>
+	                    		<div class="row detail-info">
+	                    			<div class="col-xs-12">
+										<ul class="nav nav-tabs">
+											<li role="presentation" data-nav-info="item-description" class="half"><a>Product Info</a></li>
+											<li role="presentation" data-nav-info="shipping-details" class="half active"><a>Shipping Info</a></li>
+										</ul>
+									</div>
+
+									<div class="item-description" style="display:none">
+										<p> <span class="detail-material">30% Hemp + 70% Organic Cotton</span></br><span class="detail-brief">Screen-printed feather (water-based ink)</span></p>
+									</div>
+
+									<div class="shipping-details">
+										<p>
+											If for any reason you are not happy with your purchase, you may return or exchange the item(s) within 30 days of receiving the order. Please note that monogrammed, personalized, special-order items, and items damaged through normal wear and tear are not eligible for return or exchange.
+										</p>
+									</div>
+	                    		</div>
+
+	                    		<div class="row exit-detail-xs hidden-md hidden-lg">
+	                    			<div class="col-xs-12">
+	                    				<button class="btn btn-exit-detail">Exit</button>
+	                    			</div>
+	                    		</div>
+
+	                    	</div>
+	                    </div>
+	                </div>
+	            </div>
+	        </div>
+	    </div>
+
 		<script src="/res/js/modernizr.custom.js"></script>
 		<script src="/res/js/masonry.pkgd.min.js"></script>
 		<script src="/res/js/imagesloaded.js"></script>
 		<script src="/res/js/classie.js"></script>
 		<script src="/res/js/AnimOnScroll.js"></script>
+		<script src="/res/js/AnimOnScroll.js"></script>
+		<script src="/res/js/jquery-1.11.1.min.js"></script>
+		<script src="/res/js/bootstrap.min.js"></script>
+		<script src="/res/js/home.js"></script>
 		<script>
 			new AnimOnScroll( document.getElementById( 'grid' ), {
 				minDuration : 0.4,
