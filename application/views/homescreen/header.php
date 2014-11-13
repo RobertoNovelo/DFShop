a0cc2ec5e7b443a3c84c717140332c4bfcb180f0
Roberto Novelo <robertod.novelo@gmail.com>
2014-11-12 18:28:31 -0600

    Home Screen Views Created
    The basic home screen views were created. These views allow php to
    build all the items preview on the home screen.

---
+++ application/views/homescreen/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+	<head>
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+		<title>DelFuego Shop</title>
+		<meta name="description" content="DelFuego Online Shop"/>
+		<meta name="keywords" content="DelFuego,Online,Shop"/>
+		<meta name="author" content="RobertoNovelo" />
+		<link rel="shortcut icon" href="../favicon.ico"> 
+		<link rel="stylesheet" type="text/css" href="/res/css/default.css" />
+		<link rel="stylesheet" type="text/css" href="/res/css/component.css" />
+		<link rel="stylesheet" type="text/css" href="/res/css/hover.css" />
+		<link rel="stylesheet" type="text/css" href="/res/css/site.css" />
+	</head>
+	<body>
+		<div class="container">
+			<!-- Top Navigation -->
+			<header>
+				<h1>DelFuego<span>Shop</span></h1>	
+				<nav class="codrops-demos">
+					<a class="menu-item current-demo">All</a>
