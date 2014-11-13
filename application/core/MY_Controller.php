0ae0abeebc768bda3c3779280d0efae30ddc3f9d
Roberto Novelo <robertod.novelo@gmail.com>
2014-11-12 18:35:30 -0600

    DelFuego Controller New Methods
    Debugging option can be set to true so that errors will be displayed in
    case of any. It also validates the user session. If is has none, it
    sets its flag to TRUE, to save the user’s browsing data from now on.

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -3,8 +3,39 @@
 
 class DF_Controller extends CI_Controller 
 {
+	protected $debug = true;
+
     public function __construct()
     {
         parent::__construct();
+        if($this->debug)
+        {
+        	$this->set_debugging();
+        }
+        $this->validate_session();
+    }
+
+    private function set_debugging()
+    {
+		error_reporting(E_ALL);
+		ini_set('display_errors', TRUE);
+		ini_set('display_startup_errors', TRUE);
+    }
+
+    private function validate_session()
+    {
+    	//Validate if user has a session
+    	if($this->session->userdata('userIsLoggedIn'))
+    	{
+    		//User data exists.
+    	}
+    	else//User has no session
+    	{
+    		//Initialize user session and set logged in flag.
+    		$userData = array
+    		(
+    			'userIsLoggedIn' => TRUE
+    		);
+    	}
     }
 }
